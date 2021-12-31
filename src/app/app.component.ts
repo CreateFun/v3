27fceb3a9af5bdd25e99eb904b2366a6aea15123
CreateFun <96875517+CreateFun@users.noreply.github.com>
2022-01-01 00:06:26 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,8 +12,102 @@ export class AppComponent {
   Maths()
   {
     this.show[0]=true;
+    for(let i=0;i<=10;i++)
+    {
+      if(i!==0)
+        this.show[i]=false;
+    }
    
   }
+  Food()
+  {
+    this.show[1]=true;
+    for(let i=0;i<=10;i++)
+    {
+      if(i!==1)
+        this.show[i]=false;
+    }
+   
+  }
+
+  Bhajans()
+  {
+    this.show[2]=true;
+    for(let i=0;i<=10;i++)
+    {
+      if(i!==2)
+        this.show[i]=false;
+    }
+   
+  }
+
+  BookSummaries()
+  {
+    this.show[3]=true;
+    for(let i=0;i<=10;i++)
+    {
+      if(i!==3)
+        this.show[i]=false;
+    }
+   
+  }
+
+  Kannada()
+  {
+    this.show[4]=true;
+    for(let i=0;i<=10;i++)
+    {
+      if(i!==4)
+        this.show[i]=false;
+    }
+   
+  }
+  
+  MemoryTips()
+  {
+    this.show[5]=true;
+    for(let i=0;i<=10;i++)
+    {
+      if(i!==5)
+        this.show[i]=false;
+    }
+   
+  }
+  
+  Programming()
+  {
+    this.show[6]=true;
+    for(let i=0;i<=10;i++)
+    {
+      if(i!==6)
+        this.show[i]=false;
+    }
+   
+  }
+  
+  Books()
+  {
+    this.show[7]=true;
+    for(let i=0;i<=10;i++)
+    {
+      if(i!==7)
+        this.show[i]=false;
+    }
+   
+  }
+  ContactUS()
+  {
+    this.show[8]=true;
+    for(let i=0;i<=10;i++)
+    {
+      if(i!==8)
+        this.show[i]=false;
+    }
+
+  }
+
+
+
   navbarOpen = false;
   showIcon=false;
   toggleNavbar() {
