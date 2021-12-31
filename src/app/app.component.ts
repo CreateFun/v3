14aa1e6f85af74ad264eee71c192d04bd9149760
CreateFun <96875517+CreateFun@users.noreply.github.com>
2021-12-31 21:38:58 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'createfun1';
+  title = 'createfun';
+  showMathsButton=true;
+  showMaths=false;
+  Maths()
+  {
+    this.showMaths=true;
+    this.showMathsButton=false;
+  }
 }
