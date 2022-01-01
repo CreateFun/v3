9124e7a9cbbc7f6dce2f8340c3b4c698222b7a0c
CreateFun <96875517+CreateFun@users.noreply.github.com>
2022-01-01 08:33:25 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,7 +8,9 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'createfun';
   showMathsButton=true;
-  show:boolean []=[false,false,false,false,false,false,false,false,false,false,false];
+  show:boolean []=[false,false,false,false,false,false,false,false,false,false,true];
+  createfun()
+  {}
   Maths()
   {
     this.show[0]=true;
@@ -32,13 +34,73 @@ export class AppComponent {
 
   Bhajans()
   {
-    this.show[2]=true;
+   
     for(let i=0;i<=10;i++)
     {
-      if(i!==2)
+      if(i==2 || i==9)
+      {
+        this.show[2]=true;
+        this.show[9]=true;
+      }
+      else
         this.show[i]=false;
     }
     this.navbarOpen =false;
+
+    this.url=["https://www.youtube.com/embed/SEfr-fDloHk"];
+    this.urlSafe=[this.sanitizer.bypassSecurityTrustResourceUrl(this.url[0])];
+    this. groups=[
+      {
+        "imgsrc":this.urlSafe[0],
+        "name": "Krishna Bhajan ",
+        "cardtextHeader": "Learning Maths is Fun  ",
+        "cardtextBody":  "Learning Maths is Fun",
+        "cardtextFooter":  "Learning Maths is Fun",
+        "buttonName":"Maths",
+        "link":"/maths",
+        "color":"table-primary"
+      },
+      {
+        "imgsrc":this.urlSafe[0],
+        "name": "Krishna Bhajan ",
+        "cardtextHeader": "Learning Maths is Fun  ",
+        "cardtextBody":  "Learning Maths is Fun",
+        "cardtextFooter":  "Learning Maths is Fun",
+        "buttonName":"Maths",
+        "link":"/maths",
+        "color":"table-primary"
+      },
+      {
+        "imgsrc":this.urlSafe[0],
+        "name": "Krishna Bhajan ",
+        "cardtextHeader": "Learning Maths is Fun  ",
+        "cardtextBody":  "Learning Maths is Fun",
+        "cardtextFooter":  "Learning Maths is Fun",
+        "buttonName":"Maths",
+        "link":"/maths",
+        "color":"table-primary"
+      },
+      {
+        "imgsrc":this.urlSafe[0],
+        "name": "Krishna Bhajan ",
+        "cardtextHeader": "Learning Maths is Fun  ",
+        "cardtextBody":  "Learning Maths is Fun",
+        "cardtextFooter":  "Learning Maths is Fun",
+        "buttonName":"Maths",
+        "link":"/maths",
+        "color":"table-primary"
+      },
+      {
+        "imgsrc":this.urlSafe[0],
+        "name": "Krishna Bhajan ",
+        "cardtextHeader": "Learning Maths is Fun  ",
+        "cardtextBody":  "Learning Maths is Fun",
+        "cardtextFooter":  "Learning Maths is Fun",
+        "buttonName":"Maths",
+        "link":"/maths",
+        "color":"table-primary"
+      }
+    ]
   }
 
   BookSummaries()
@@ -54,13 +116,28 @@ export class AppComponent {
 
   Kannada()
   {
-    this.show[4]=true;
+    this.show[9]=true;
     for(let i=0;i<=10;i++)
     {
-      if(i!==4)
+      if(i!==9)
         this.show[i]=false;
     }
     this.navbarOpen =false;
+    this.url=["https://www.youtube.com/embed/SEfr-fDloHk"];
+    this.urlSafe=[this.sanitizer.bypassSecurityTrustResourceUrl(this.url[0])];
+    this. groups=[
+      {
+        "imgsrc":this.urlSafe[0],
+        "name": "Maths",
+        "cardtextHeader": "Learning Maths is Fun  ",
+        "cardtextBody":  "Learning Maths is Fun",
+        "cardtextFooter":  "Learning Maths is Fun",
+        "buttonName":"Maths",
+        "link":"/maths",
+        "color":"table-primary"
+      }]
+
+
   }
   
   MemoryTips()
@@ -105,8 +182,50 @@ export class AppComponent {
     }
     this.navbarOpen =false;
   }
+  // About Table starts 
+  url: string[] =["https://www.youtube.com/embed/SEfr-fDloHk","https://www.youtube.com/embed/SEfr-fDloHk","https://www.youtube.com/embed/SEfr-fDloHk","https://www.youtube.com/embed/SEfr-fDloHk","https://www.youtube.com/embed/SEfr-fDloHk"];
+  urlSafe: SafeResourceUrl[]=[this.sanitizer.bypassSecurityTrustResourceUrl(this.url[0]),this.sanitizer.bypassSecurityTrustResourceUrl(this.url[1])]
+  /*
+  urlSafe1: SafeResourceUrl= this.sanitizer.bypassSecurityTrustResourceUrl(this.url[0]);
+  urlSafe2: SafeResourceUrl= this.sanitizer.bypassSecurityTrustResourceUrl(this.url[1]);
+  urlSafe3: SafeResourceUrl= this.sanitizer.bypassSecurityTrustResourceUrl(this.url[2]);
+  urlSafe4: SafeResourceUrl= this.sanitizer.bypassSecurityTrustResourceUrl(this.url[3]);
+  urlSafe5: SafeResourceUrl= this.sanitizer.bypassSecurityTrustResourceUrl(this.url[4]);      */
 
+  groups=[
+    {
+      "imgsrc":this.urlSafe[0],
+      "name": "Maths",
+      "cardtextHeader": "Learning Maths is Fun  ",
+      "cardtextBody":  "Learning Maths is Fun",
+      "cardtextFooter":  "Learning Maths is Fun",
+      "buttonName":"Maths",
+      "link":"/maths",
+      "color":"table-primary"
+    },
+    {
+      "imgsrc":this.urlSafe[1],
+      "name": "Bhajans",
+      "cardtextHeader": "Learning Maths is Fun  ",
+        "cardtextBody":  "Learning Maths is Fun",
+        "cardtextFooter":  "Learning Maths is Fun",
+      "buttonName":"Bhajans",
+      "link":"/bhajans",
+      "color":"table-success"
+    }];
+    constructor(public sanitizer: DomSanitizer) {
 
+ 
+    }
+  Demo(){
+    this.show[9]=true;
+    for(let i=0;i<=10;i++)
+    {
+      if(i!==9)
+        this.show[i]=false;
+    }
+    this.navbarOpen =false;
+  }
 
   navbarOpen = false;
   showIcon=false;
